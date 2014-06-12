88af63366dad51ca57995c8165b8bb768192858c
fassad1001 <fassad1001@yandex.ru>
2014-06-12 10:32:01 +0400

    была написана функция qHash но она почему-то возвращает 0 каждый раз!

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include <QCoreApplication>
 
+#include <QCryptographicHash>
+
 #include "TTriples.h"
+
+inline uint qHash(const Triple &triple)
+{
+    return (QCryptographicHash::hash(QByteArray().append(triple.toString()),QCryptographicHash::Sha3_384).toUInt());
+}
 
 int main(int argc, char *argv[])
 {
+    // этот гадёныш всегда ноль возвращает! =(
+//    qWarning()<<(QCryptographicHash::hash(QByteArray().append("fewfdwfds"),QCryptographicHash::Sha256).toUInt());
+
     QCoreApplication a(argc, argv);
 
-//    TTriplesStorage tTriplesStorage;
-//    QTest::qExec(&tTriplesStorage);
     TTriples Ttriples;
     QTest::qExec(&Ttriples);
-
 
     return 0;
 }
