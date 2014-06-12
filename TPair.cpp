7ef4117c2b8e877c52ee24fcb8f6aeb3dec4fd51
fassad1001 <fassad1001@yandex.ru>
2014-06-12 15:14:09 +0400

    были добавлены тесты с пустыми телами для класса Pair

--- TPair.cpp
+++ TPair.cpp
@@ -3,3 +3,43 @@
 TPair::TPair()
 {
 }
+
+void TPair::testSubjectAndPredicatesFor()
+{
+
+}
+
+void TPair::testSubjectAndPredicatesFor_data()
+{
+
+}
+
+void TPair::testPredicatesAndObjects()
+{
+
+}
+
+void TPair::testPredicatesAndObjects_data()
+{
+
+}
+
+void TPair::testSubjectsAndObjects()
+{
+
+}
+
+void TPair::testSubjectsAndObjects_data()
+{
+
+}
+
+void TPair::testContains()
+{
+
+}
+
+void TPair::testContains_data()
+{
+
+}
--- TPair.h
+++ TPair.h
@@ -1,10 +1,29 @@
 #ifndef TPAIR_H
+
 #define TPAIR_H
 
-class TPair
+#include "Pair.h"
+
+#include <QtTest/QtTest>
+
+class TPair: public QObject
 {
+    Q_OBJECT
 public:
     TPair();
+private slots:
+    //тесты
+    void testSubjectAndPredicatesFor();
+    void testSubjectAndPredicatesFor_data();
+
+    void testPredicatesAndObjects();
+    void testPredicatesAndObjects_data();
+
+    void testSubjectsAndObjects();
+    void testSubjectsAndObjects_data();
+
+    void testContains();
+    void testContains_data();
 };
 
 #endif // TPAIR_H
