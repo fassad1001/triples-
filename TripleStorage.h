13a5a7271bcaccd2173185d6c37fe1d94164fc97
fassad1001 <fassad1001@yandex.ru>
2014-06-12 13:14:06 +0400

    в класс triplestorage были добавлены конструктор с аргументом.

--- TripleStorage.h
+++ TripleStorage.h
@@ -7,13 +7,14 @@
 class TripleStorage
 {
 public:
+    TripleStorage(const QSet<Triple> &triples);
     TripleStorage();
     void addTriple(const Triple& triple);
     QSet<QString> objectsFor(const QString &subject, const QString &predicate);
     QSet<QString> subjectsFor(const QString &subject, const QString &predicate);
     QSet<QString> predicatesFor(const QString &subject, const QString &predicate);
 private:
-    QSet<Triple> triples;
+    QSet<Triple> triples_;
 };
 
 #endif // TRIPLESTORAGE_H
