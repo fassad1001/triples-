c27905328d3bba508efdeb1e5e364490908dd303
fassad1001 <fassad1001@yandex.ru>
2014-06-12 13:44:12 +0400

    был добавлен оператор == в класс Pair.h

--- Pair.h
+++ Pair.h
@@ -1,10 +1,31 @@
 #ifndef PAIR_H
 #define PAIR_H
-
+#include <QtCore>
+#include "Triple.h"
 class Pair
 {
 public:
     Pair();
+    Pair(const QString &first, const QString &second);
+    QSet<Pair> subjectAndPredicatesFor(const QString &object);
+    QSet<Pair> predicatesAndObjects(const QString &object);
+    QSet<Pair> subjectsAndObjects(const QString &object);
+    bool contains(const Triple &triple);
+    bool operator ==(Pair &p)
+    {
+        if (first_ == p.first_ && second_ == p.second_)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+private:
+    QString first_;
+    QString second_;
 };
 
 #endif // PAIR_H
