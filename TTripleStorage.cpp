65ffbd4827599e5165008631934d4acc8599bcdc
fassad1001 <fassad1001@yandex.ru>
2014-06-17 17:27:47 +0400

    были добавлены тесты для функции objectsFor

--- TTripleStorage.cpp
+++ TTripleStorage.cpp
@@ -49,12 +49,90 @@ void TTripleStorage::testContains_data()
 
 void TTripleStorage::testObjectsFor()
 {
+    QFETCH(TripleStorage, input0);
+    QFETCH(QString, input1);
+    QFETCH(QString, input2);
 
+    QFETCH(QSet<QString>, output);
+
+    QCOMPARE(input0.objectsFor(input1,input2),output);
 }
 
 void TTripleStorage::testObjectsFor_data()
 {
+    QTest::addColumn <TripleStorage> ("input0");
+    QTest::addColumn <QString> ("input1");
+    QTest::addColumn <QString> ("input2");
 
+    QTest::addColumn <QSet<QString> > ("output");
+
+    QTest::newRow("blank = true") << (TripleStorage(QSet<Triple>()
+                                        <<Triple("","","")
+                                        <<Triple("","","")
+                                        <<Triple("","","")
+                                        <<Triple("","","")
+                                        <<Triple("","","")
+                                        <<Triple("","","")))
+                                          <<("")
+                                            <<("")
+                                              <<(QSet<QString>()
+                                                 <<""
+                                                 <<""
+                                                 <<""
+                                                 <<""
+                                                 <<""
+                                                 <<"");
+
+    QTest::newRow("not blank = false") << (TripleStorage(QSet<Triple>()
+                                        <<Triple("5","5","1")
+                                        <<Triple("5","5","2")
+                                        <<Triple("5","5","3")
+                                        <<Triple("5","5","4")
+                                        <<Triple("5","5","5")
+                                        ))
+                                          <<("5")
+                                            <<("5")
+                                              <<(QSet<QString>()
+                                                 <<"1"
+                                                 <<"2"
+                                                 <<"3"
+                                                 <<"4"
+                                                 <<"5");
+
+    QTest::newRow("not blank = true") << (TripleStorage(QSet<Triple>()
+                                        <<Triple("1","5","5")
+                                        <<Triple("2","5","5")
+                                        <<Triple("3","5","5")
+                                        <<Triple("4","5","5")
+                                        <<Triple("5","5","5")
+                                        ))
+                                          <<("5")
+                                            <<("5")
+                                              <<(QSet<QString>()
+                                                 <<"5");
+
+    QTest::newRow("blank & not blank = true") << (TripleStorage(QSet<Triple>()
+                                        <<Triple("1","","5")
+                                        <<Triple("2","5","")
+                                        <<Triple("3","5","5")
+                                        <<Triple("","5","5")
+                                        <<Triple("5","5","5")
+                                        ))
+                                          <<("5")
+                                            <<("5")
+                                              <<(QSet<QString>()
+                                                 <<"5");
+
+    QTest::newRow("blank & not blank = true") << (TripleStorage(QSet<Triple>()
+                                        <<Triple("1","","5")
+                                        <<Triple("2","5","")
+                                        <<Triple("3","5","5")
+                                        <<Triple("","5","5")
+                                        <<Triple("5","5","5")
+                                        ))
+                                          <<("4")
+                                            <<("5")
+                                              <<(QSet<QString>());
 }
 
 void TTripleStorage::testSubjectsFor()
