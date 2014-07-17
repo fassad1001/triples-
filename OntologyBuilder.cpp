610d84b48e3bbe6b5951e08bfb42f8ed99e10a40
fassad1001 <fassad1001@yandex.ru>
2014-07-17 15:06:26 +0400

    исправлен метод addProperty для прохождения всех тестов

--- OntologyBuilder.cpp
+++ OntologyBuilder.cpp
@@ -6,15 +6,16 @@ OntologyBuilder::OntologyBuilder()
 
 void OntologyBuilder::addClass(const QString &className, const QString &parentClassName)
 {
+    QSet<QString> allClassItems = allClasses();
     //если все классы содержат вводный класс
-    if(allClasses().contains(className))
+    if(allClassItems.contains(className))
     {
         //return;
         return;
     }
     if(parentClassName != QString())
     {
-        if(!allClasses().contains(parentClassName))
+        if(!allClassItems.contains(parentClassName))
         {
             return;
         }
@@ -24,12 +25,36 @@ void OntologyBuilder::addClass(const QString &className, const QString &parentCl
     //если parentClassName != QString()
     if(parentClassName != QString())
     {
-        if(allClasses().contains(parentClassName))
+        if(allClassItems.contains(parentClassName))
         {
             //добавить (имя_родительского_класса,CONTAINS,имя_класса)
             add(parentClassName, Ontology::CONTAINS, className);
         }
     }
+}
+
+void OntologyBuilder::addProperty(const QString &className, const QString &propertyName)
+{
+    //если все классы содержат класс имя_класса
+    if(allClasses().contains(className))
+    {
+        //для каждого класса
+        foreach(QString classItem, allClasses())
+        {
+            //если класс содержит свойство имя_свойства
+            if(classProperties(classItem).contains(propertyName))
+            {
+                //return;
+                return;
+            }
+        }
+    }
+    else
+    {
+        return;
+    }
+    //добавить (имя_класса, HAS_PROPERTY, имя_свойства)
+    add(className, Ontology::HAS_PROPERTY, propertyName);
 }
 
 Ontology OntologyBuilder::getOntology() const
--- OntologyBuilder.h
+++ OntologyBuilder.h
@@ -9,6 +9,8 @@ class OntologyBuilder : public Ontology
 public:
     OntologyBuilder();
     void addClass(const QString &className, const QString &parentClassName = QString());
+    void addProperty(const QString &className, const QString &propertyName);
+
     Ontology getOntology() const;
 
 };
