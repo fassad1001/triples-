3d6c9a9d97baf5c5b8e7e1a87ffc65f22c64e818
fassad1001 <fassad1001@yandex.ru>
2014-07-17 15:05:18 +0400

    все написанные тесты для addProperty полность пройденны

--- TOntologyBuilder.cpp
+++ TOntologyBuilder.cpp
@@ -129,3 +129,186 @@ void TOntologyBuilder::TestAddClass()
 
     QCOMPARE(BuilderOntology, checkOntology);
 }
+
+void TOntologyBuilder::TestAddProperty_data()
+{
+    QTest::addColumn <Ontology> ("BuilderOntology");
+    QTest::addColumn <Ontology> ("checkOntology");
+
+    OntologyBuilder ontologyBuilder;
+    ontologyBuilder.addClass("class1");
+    ontologyBuilder.addProperty("class1", "property1");
+
+    Ontology checkOntology;
+    checkOntology.add("class1", Ontology::IS, Ontology::CLASS);
+    checkOntology.add("class1", Ontology::HAS_PROPERTY, "property1");
+
+    QTest::newRow("1 class, 1 property")
+            << (ontologyBuilder.getOntology())
+            << (checkOntology);
+
+    ontologyBuilder.clearStorage();
+    ontologyBuilder.addClass("class1");
+    ontologyBuilder.addProperty("class1", "property1");
+    ontologyBuilder.addProperty("class1", "property2");
+
+    checkOntology.clearStorage();
+    checkOntology.add("class1", Ontology::IS, Ontology::CLASS);
+    checkOntology.add("class1", Ontology::HAS_PROPERTY, "property1");
+    checkOntology.add("class1", Ontology::HAS_PROPERTY, "property2");
+
+    QTest::newRow("1 class, 2 properties")
+            << (ontologyBuilder.getOntology())
+            << (checkOntology);
+
+    QTest::newRow("1 class, 1 property")
+            << (ontologyBuilder.getOntology())
+            << (checkOntology);
+
+    ontologyBuilder.clearStorage();
+    ontologyBuilder.addClass("class1");
+    ontologyBuilder.addProperty("class1", "property1");
+    ontologyBuilder.addProperty("class1", "property2");
+
+    ontologyBuilder.addClass("class2");
+    ontologyBuilder.addProperty("class2", "property3");
+    ontologyBuilder.addProperty("class2", "property4");
+
+    checkOntology.clearStorage();
+    checkOntology.add("class1", Ontology::IS, Ontology::CLASS);
+    checkOntology.add("class1", Ontology::HAS_PROPERTY, "property1");
+    checkOntology.add("class1", Ontology::HAS_PROPERTY, "property2");
+
+    checkOntology.add("class2", Ontology::IS, Ontology::CLASS);
+    checkOntology.add("class2", Ontology::HAS_PROPERTY, "property3");
+    checkOntology.add("class2", Ontology::HAS_PROPERTY, "property4");
+
+    QTest::newRow("2 class, 4 properties, 2 header")
+            << (ontologyBuilder.getOntology())
+            << (checkOntology);
+
+    ontologyBuilder.clearStorage();
+    ontologyBuilder.addClass("class1");
+    ontologyBuilder.addClass("class11", "class1");
+    ontologyBuilder.addProperty("class11", "property1");
+    ontologyBuilder.addProperty("class11", "property2");
+
+    ontologyBuilder.addClass("class2");
+    ontologyBuilder.addClass("class22", "class2");
+    ontologyBuilder.addProperty("class22", "property3");
+    ontologyBuilder.addProperty("class22", "property4");
+
+    checkOntology.clearStorage();
+    checkOntology.add("class1", Ontology::IS, Ontology::CLASS);
+    checkOntology.add("class11", Ontology::IS, Ontology::CLASS);
+    checkOntology.add("class1", Ontology::CONTAINS, "class11");
+    checkOntology.add("class11", Ontology::HAS_PROPERTY, "property1");
+    checkOntology.add("class11", Ontology::HAS_PROPERTY, "property2");
+
+    checkOntology.add("class2", Ontology::IS, Ontology::CLASS);
+    checkOntology.add("class22", Ontology::IS, Ontology::CLASS);
+    checkOntology.add("class2", Ontology::CONTAINS, "class22");
+    checkOntology.add("class22", Ontology::HAS_PROPERTY, "property3");
+    checkOntology.add("class22", Ontology::HAS_PROPERTY, "property4");
+
+    QTest::newRow("4 class, 4 properties, 2 header")
+            << (ontologyBuilder.getOntology())
+            << (checkOntology);
+
+    ontologyBuilder.clearStorage();
+    ontologyBuilder.addClass("class1");
+    ontologyBuilder.addClass("class11", "class1");
+    ontologyBuilder.addProperty("class11", "property1");
+    ontologyBuilder.addProperty("class111", "property2");
+
+    ontologyBuilder.addClass("class2");
+    ontologyBuilder.addClass("class22", "class2");
+    ontologyBuilder.addProperty("class22", "property3");
+    ontologyBuilder.addProperty("class222", "property4");
+
+    checkOntology.clearStorage();
+    checkOntology.add("class1", Ontology::IS, Ontology::CLASS);
+    checkOntology.add("class11", Ontology::IS, Ontology::CLASS);
+    checkOntology.add("class1", Ontology::CONTAINS, "class11");
+    checkOntology.add("class11", Ontology::HAS_PROPERTY, "property1");
+
+    checkOntology.add("class2", Ontology::IS, Ontology::CLASS);
+    checkOntology.add("class22", Ontology::IS, Ontology::CLASS);
+    checkOntology.add("class2", Ontology::CONTAINS, "class22");
+    checkOntology.add("class22", Ontology::HAS_PROPERTY, "property3");
+
+    QTest::newRow("2 class, 4 properties, 2 header, add property with fake class name")
+            << (ontologyBuilder.getOntology())
+            << (checkOntology);
+
+    ontologyBuilder.clearStorage();
+    ontologyBuilder.addClass("class1");
+    ontologyBuilder.addClass("class11", "class1");
+    ontologyBuilder.addProperty("class11", "property1");
+    ontologyBuilder.addProperty("class11", "property2");
+    ontologyBuilder.addProperty("class1", "property2");
+
+
+    ontologyBuilder.addClass("class2");
+    ontologyBuilder.addClass("class22", "class2");
+    ontologyBuilder.addProperty("class22", "property3");
+    ontologyBuilder.addProperty("class222", "property4");
+    ontologyBuilder.addProperty("class22", "property4");
+
+    checkOntology.clearStorage();
+    checkOntology.add("class1", Ontology::IS, Ontology::CLASS);
+    checkOntology.add("class11", Ontology::IS, Ontology::CLASS);
+    checkOntology.add("class1", Ontology::CONTAINS, "class11");
+    checkOntology.add("class11", Ontology::HAS_PROPERTY, "property1");
+    checkOntology.add("class11", Ontology::HAS_PROPERTY, "property2");
+
+    checkOntology.add("class2", Ontology::IS, Ontology::CLASS);
+    checkOntology.add("class22", Ontology::IS, Ontology::CLASS);
+    checkOntology.add("class2", Ontology::CONTAINS, "class22");
+    checkOntology.add("class22", Ontology::HAS_PROPERTY, "property3");
+    checkOntology.add("class22", Ontology::HAS_PROPERTY, "property4");
+
+    QTest::newRow("2 class, 4 properties, 2 header, add property adrealy exists")
+            << (ontologyBuilder.getOntology())
+            << (checkOntology);
+
+    ontologyBuilder.clearStorage();
+    ontologyBuilder.addClass("class1");
+    ontologyBuilder.addClass("class11", "class1");
+    ontologyBuilder.addProperty("class11", "property1");
+    ontologyBuilder.addProperty("class11", "property2");
+    ontologyBuilder.addProperty("class1", "property2");
+
+
+    ontologyBuilder.addClass("class2");
+    ontologyBuilder.addClass("class22", "class2");
+    ontologyBuilder.addProperty("class22", "property3");
+    ontologyBuilder.addProperty("class222", "property4");
+    ontologyBuilder.addProperty("class22", "property4");
+
+    checkOntology.clearStorage();
+    checkOntology.add("class1", Ontology::IS, Ontology::CLASS);
+    checkOntology.add("class11", Ontology::IS, Ontology::CLASS);
+    checkOntology.add("class1", Ontology::CONTAINS, "class11");
+    checkOntology.add("class11", Ontology::HAS_PROPERTY, "property1");
+    checkOntology.add("class11", Ontology::HAS_PROPERTY, "property2");
+
+    checkOntology.add("class2", Ontology::IS, Ontology::CLASS);
+    checkOntology.add("class22", Ontology::IS, Ontology::CLASS);
+    checkOntology.add("class2", Ontology::CONTAINS, "class22");
+    checkOntology.add("class22", Ontology::HAS_PROPERTY, "property3");
+    checkOntology.add("class22", Ontology::HAS_PROPERTY, "property4");
+
+    QTest::newRow("2 class, 4 properties, 2 header, addproperty use fake class and add property adrealy exists")
+            << (ontologyBuilder.getOntology())
+            << (checkOntology);
+
+}
+
+void TOntologyBuilder::TestAddProperty()
+{
+    QFETCH(Ontology, BuilderOntology);
+    QFETCH(Ontology, checkOntology);
+
+    QCOMPARE(BuilderOntology, checkOntology);
+}
--- TOntologyBuilder.h
+++ TOntologyBuilder.h
@@ -15,6 +15,9 @@ private slots:
     void TestAddClass_data();
     void TestAddClass();
 
+    void TestAddProperty_data();
+    void TestAddProperty();
+
 
 };
 
