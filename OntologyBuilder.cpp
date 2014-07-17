22b75254772ee8bb4cbe5fb3bb0aa687697d8daf
fassad1001 <fassad1001@yandex.ru>
2014-07-17 12:27:04 +0400

    добавлена реализация метода addClass

--- OntologyBuilder.cpp
+++ OntologyBuilder.cpp
@@ -13,7 +13,11 @@ void OntologyBuilder::addClass(const QString &className, const QString &parentCl
         return;
     }
     //добавить (имя_класса, IS, CLASS)
-
+    add(className, Ontology::IS, Ontology::CLASS);
     //если parentClassName != QString()
+    if(parentClassName != QString())
+    {
         //добавить (имя_родительского_класса,CONTAINS,имя_класса)
+        add(parentClassName, Ontology::CONTAINS, className);
+    }
 }
