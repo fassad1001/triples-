0385e8ae9e8aa7aed79428ca71701ddf54474f15
fassad1001 <fassad1001@yandex.ru>
2014-07-19 12:08:35 +0400

    написан и проверен на работоспособность метод generate

--- OntologyGenerator.cpp
+++ OntologyGenerator.cpp
@@ -13,14 +13,17 @@ Ontology OntologyGenerator::generate(const int ClassCountSummary
     QString className;
     QString instanceName;
     QString propertyName;
+    QSet<QString> classPropertiesItems;
+    QSet<QString> classInstancesItems;
     bool canGenerate = true;
     //пока могу генерировать:
     while(canGenerate)
     {
         //если классы нельзы создавать
         //если суммарное количество классов > лимита
-        if(allClasses().count() > classesSummary)
+        if(allClasses().count() >= ClassCountSummary)
         {
+            qWarning()<<"классов достаточно";
             //прервать
             break;
         }
@@ -31,6 +34,7 @@ Ontology OntologyGenerator::generate(const int ClassCountSummary
             className = generateClass();
             //создать случайный класс c надклассом в виде случайного класса
             addClass(className, allClasses().toList().at(qrand() % allClasses().count()));
+            qWarning()<<">>создаю класс с подклассом";
         }
         //иначе
         else
@@ -38,38 +42,61 @@ Ontology OntologyGenerator::generate(const int ClassCountSummary
             //создать случайный класс без надкласса
             className = generateClass();
             addClass(className);
+            qWarning()<<">>создаю класс без подкласса";
         }
         //пока классу можно добавлять свойства
         //пока кол-во свойств у классов <= общего предела свойств классов
-        while(classProperties(className).count() <= classPropertiesCount)
+        while(classProperties(className).count() < classPropertiesCount)
         {
             //генерировать свойства для класса
             propertyName = generateProperty();
             //заполнить свойство класса значением
             addProperty(className, propertyName);
+            qWarning()<<"создаю свойство";
         }
 
         //пока инстансы можно создавать
-        //пока количество инстансов <= общего предела инстансов
-        while(allInstances() <= instancesCountSummary)
+        //пока количество инстансов < общего предела инстансов
+        qWarning()<<"проверяю, "<<allInstances().count()<<"<"<<instancesCountSummary;
+        if(allInstances().count() > instancesCountSummary)
+        {
+            qWarning()<<"я не могу создавать инстансы";
+        }
+        else
+        {
+            qWarning()<<"начинаю создавать инстансы для класса "<<className;
+        }
+        while(allInstances().count() < instancesCountSummary)
         {
             //если для класса можно добавить инстанс
-            //если количество инстансов для класса <= предела инстансов для одного класса
-            if(classInstances(className).count() <= ClassCountSummary)
+            //если количество инстансов класса < предела инстансов для одного класса
+            if(classInstances(className).count() < classInstancesCount)
             {
                 //генерировать инстанс для класса
                 instanceName = generateInstance();
                 addInstance(className, instanceName);
-                setPropertyValue();
+                qWarning()<<"создаю инстанс"<<instanceName<<" для "<<className;
             }
             //иначе
             else
             {
                 //выход из цикла
+                qWarning()<<"прекращаю создавать инстансы";
                 break;
+
             }
         }
+        qWarning()<<"у класса "<<className<<" есть инстансы "<<classInstances(className);
 
+    }
+    classInstancesItems = classInstances(className);
+    classPropertiesItems = classProperties(className);
+    foreach(QString classInstancesItem, classInstancesItems)
+    {
+        foreach(QString classPropertiesItem, classPropertiesItems)
+        {
+            setPropertyValue(classInstancesItem, classPropertiesItem, generatePropertyValue());
+        }
     }
     return getOntology();
 }
@@ -89,7 +116,7 @@ QString OntologyGenerator::generateProperty() const
     return QString("propeertyN") + QString::number(qrand());
 }
 
-QString OntologyGenerator::generatePtopertyValue() const
+QString OntologyGenerator::generatePropertyValue() const
 {
     return QString("propeertyValueN") + QString::number(qrand());
 }
